8ef30ffda179d9d078870f54b0d716e01118e5b7
Viktor Ristic <vktr.r2@protonmail.com>
2023-07-15 23:48:02 -0400

    Add base readline question for user input

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
+var readline = require("readline");
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+//Declare taskList array to store Task objects w/ two default tasks
 var taskList = [
-    { id: 1, title: "Test", status: "Incomplete", dueDate: new Date("2023-07-25") },
-    { id: 2, title: "Do Laundry", status: "Incomplete", dueDate: new Date("2023-07-15") }
+    {
+        id: 1,
+        title: "Test",
+        status: "Incomplete",
+        dueDate: new Date("2023-07-25"),
+    },
+    {
+        id: 2,
+        title: "Do Laundry",
+        status: "Incomplete",
+        dueDate: new Date("2023-07-15"),
+    },
 ];
-console.log(taskList);
-console.log(taskList[1]);
+rl.question(" \n Welcome to TS Task Manager.  Please enter one of the following options: \n\n1 - View current tasks \n\n2 - Add new task \n\n3 - Edit existing task \n\n4 - Mark task as completed \n\n5 - Delete task \n\n", function (input) {
+    input === "1" && console.log(taskList);
+    input === "2" && console.log("Input is 2");
+    input === "3" && console.log("Input is 3");
+    input === "4" && console.log("Input is 4");
+    input === "5" && console.log("Input is 5");
+});
+// console.log(taskList);
+// console.log(taskList[1]);
--- index.ts
+++ index.ts
@@ -1,3 +1,9 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
 //Declare custom Task type
 type Task = {
@@ -5,14 +11,40 @@ type Task = {
   title: string;
   status: string;
   dueDate: Date;
-}
+};
 
-//Declare taskList array to store Task objects
+//Declare taskList array to store Task objects w/ two default tasks
 const taskList: Task[] = [
-  {id: 1, title: "Test", status: "Incomplete", dueDate: new Date("2023-07-25")},
-  {id: 2, title: "Do Laundry", status: "Incomplete", dueDate: new Date("2023-07-15")}
-]
+  {
+    id: 1,
+    title: "Test",
+    status: "Incomplete",
+    dueDate: new Date("2023-07-25"),
+  },
+  {
+    id: 2,
+    title: "Do Laundry",
+    status: "Incomplete",
+    dueDate: new Date("2023-07-15"),
+  },
+];
 
-//
+rl.question(
+  ` \n Welcome to TS Task Manager.  Please enter one of the following options: \n
+1 - View current tasks \n
+2 - Add new task \n
+3 - Edit existing task \n
+4 - Mark task as completed \n
+5 - Delete task \n
+`,
+  (input: string): void => {
+    input === "1" && console.log(taskList);
+    input === "2" && console.log("Input is 2");
+    input === "3" && console.log("Input is 3");
+    input === "4" && console.log("Input is 4");
+    input === "5" && console.log("Input is 5");
+  }
+);
+
 // console.log(taskList);
 // console.log(taskList[1]);
