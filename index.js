7b68ab812f18576bb29e1c7c7966dc7cd655cbf4
Viktor Ristic <vktr.r2@protonmail.com>
2023-07-16 00:21:43 -0400

    Refactor createTask to use helper function

--- index.js
+++ index.js
@@ -20,17 +20,18 @@ var taskList = [
 ];
 rl.question(" \n Welcome to TS Task Manager.  Please enter one of the following options: \n\n1 - View current tasks \n\n2 - Add new task \n\n3 - Edit existing task \n\n4 - Mark task as completed \n\n5 - Delete task \n\n", function (input) {
     input === "1" && console.log(taskList);
-    input === "2" &&
-        rl.question("\nPlease provide a title\n", function (title) {
-            console.log(title);
-            rl.question("\nPlease provide a due date in the format of YYYY-MM-DD\n", function (date) {
-                console.log(new Date(date));
-            });
-        });
+    input === "2" && createTask();
     input === "3" && console.log("Input is 3");
     input === "4" && console.log("Input is 4");
     input === "5" && console.log("Input is 5");
 });
-var createTask = function (title, dueDate) { };
+var createTask = function () {
+    rl.question("\nPlease provide a title\n", function (title) {
+        console.log(title);
+        rl.question("\nPlease provide a due date in the format of YYYY-MM-DD\n", function (date) {
+            console.log(new Date(date));
+        });
+    });
+};
 // console.log(taskList);
 // console.log(taskList[1]);
--- index.ts
+++ index.ts
@@ -39,23 +39,24 @@ rl.question(
 `,
   (input: string): void => {
     input === "1" && console.log(taskList);
-    input === "2" &&
-      rl.question(`\nPlease provide a title\n`, (title: string): void => {
-        console.log(title);
-        rl.question(
-          `\nPlease provide a due date in the format of YYYY-MM-DD\n`,
-          (date: string): void => {                              
-            console.log(new Date(date));
-          }
-        );
-      });
+    input === "2" && createTask();
     input === "3" && console.log("Input is 3");
     input === "4" && console.log("Input is 4");
     input === "5" && console.log("Input is 5");
   }
 );
 
-const createTask = (title: string, dueDate: string): void => {};
+const createTask = (): void => {
+  rl.question(`\nPlease provide a title\n`, (title: string): void => {
+    console.log(title);
+    rl.question(
+      `\nPlease provide a due date in the format of YYYY-MM-DD\n`,
+      (date: string): void => {                              
+        console.log(new Date(date));
+      }
+    );
+  });
+};
 
 // console.log(taskList);
 // console.log(taskList[1]);
