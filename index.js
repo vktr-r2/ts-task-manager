8f785d1d00e1ee8f4eeaf4dad16552a35af35760
Viktor Ristic <vktr.r2@protonmail.com>
2023-07-17 16:15:37 -0400

    Modularize createTask function

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.promptOptions = exports.taskList = void 0;
 const readline = require("readline");
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
+    terminal: false,
 });
+const create_1 = require("./helpers/create");
 //Declare taskList array to store Task objects w/ two default tasks
-const taskList = [
+exports.taskList = [
     {
         id: 1,
         title: "Test",
@@ -29,49 +33,24 @@ const promptOptions = () => {
     5 - Delete task \n
     6 - Close Task Manager \n
     `, (input) => {
-        input === "1" && readTasks(taskList);
-        input === "2" && createTask();
+        input === "1" && readTasks(exports.taskList);
+        input === "2" && (0, create_1.createTask)();
         input === "3" && console.log("Input is 3");
         input === "4" && console.log("Input is 4");
         input === "5" && console.log("Input is 5");
         input === "6" && closeTaskManager();
     });
 };
-//Helper function that creates a new task
-const createTask = () => {
-    //Setup newTask object as custom Task type
-    const newTask = {
-        id: taskList.length + 1,
-        title: "",
-        status: "Incomplete",
-        dueDate: new Date(),
-    };
-    //Ask user to input title for task
-    rl.question(`\nPlease provide a title\n`, (title) => {
-        //Set user input to be title in newTask object
-        newTask.title = title;
-        //Ask user to input dueDate for task
-        rl.question(`\nPlease provide a due date in the format of YYYY-MM-DD\n`, (date) => {
-            //Set user input to be dueDate in newTask object
-            newTask.dueDate = new Date(date);
-            //Show user the task they've created
-            console.log(newTask);
-            //Push newTask object to taskList array
-            taskList.push(newTask);
-            // Loop user back to Task Manager prompt
-            promptOptions();
-        });
-    });
-};
+exports.promptOptions = promptOptions;
 const readTasks = (list) => {
     console.log(list);
-    setTimeout(() => promptOptions(), 4000);
+    setTimeout(() => (0, exports.promptOptions)(), 4000);
 };
 //Helper function that closes the Task Manager app in terminal
 const closeTaskManager = () => {
     rl.close();
 };
 console.log(`\n ~~~ Welcome to Task Manager! ~~~`);
-promptOptions();
+(0, exports.promptOptions)();
 // console.log(taskList);
 // console.log(taskList[1]);
--- index.ts
+++ index.ts
@@ -3,7 +3,11 @@ const readline = require("readline");
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
+  terminal: false,
 });
+
+import { createTask } from './helpers/create'
+
 
 //Declare custom Task type
 type Task = {
@@ -14,7 +18,7 @@ type Task = {
 };
 
 //Declare taskList array to store Task objects w/ two default tasks
-const taskList: Task[] = [
+export const taskList: Task[] = [
   {
     id: 1,
     title: "Test",
@@ -29,7 +33,7 @@ const taskList: Task[] = [
   },
 ];
 
-const promptOptions = (): void => {
+export const promptOptions = (): void => {
 
   //Trigger initial question for user to select option
   rl.question(
@@ -52,36 +56,6 @@ const promptOptions = (): void => {
     );
   };
 
-//Helper function that creates a new task
-const createTask = (): void => {
-  //Setup newTask object as custom Task type
-  const newTask: Task = {
-    id: taskList.length + 1, //Default id
-    title: "",
-    status: "Incomplete", //Default status
-    dueDate: new Date(),
-  };
-  //Ask user to input title for task
-  rl.question(`\nPlease provide a title\n`, (title: string): void => {
-    //Set user input to be title in newTask object
-    newTask.title = title;
-
-    //Ask user to input dueDate for task
-    rl.question(
-      `\nPlease provide a due date in the format of YYYY-MM-DD\n`,
-      (date: string): void => {
-        //Set user input to be dueDate in newTask object
-        newTask.dueDate = new Date(date);
-        //Show user the task they've created
-        console.log(newTask);
-        //Push newTask object to taskList array
-        taskList.push(newTask);
-        // Loop user back to Task Manager prompt
-        promptOptions();
-      }
-    );
-  });
-};
 
 const readTasks = (list: Task[]) => {
   console.log(list);
