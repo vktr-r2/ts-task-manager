738cee2c8909743cbcf705ca4fc43d3a12287c4e
Viktor Ristic <vktr.r2@protonmail.com>
2023-07-20 20:36:10 -0400

    Implement ability to edit existing task title

--- helpers/update.js
+++ helpers/update.js
@@ -9,8 +9,32 @@ const rl = readline.createInterface({
 });
 const index_1 = require("../index");
 //Helper function that edits an existing task
-const updateTask = () => {
-    console.log(index_1.taskList);
-    rl.question(`\nPlease enter a task ID\n`, (id) => { });
+const updateTask = (list) => {
+    //Show user current taskList
+    console.log(list);
+    //ASYNC TEMP WORKAROUND
+    setTimeout(() => {
+        //Ask user to input id for task to be edited
+        rl.question(`\nPlease select which task you'd like to edit\n`, (id) => {
+            console.log(list[id]);
+            rl.question(`\nUpdate title? Y or N\n`, (answer) => {
+                if (answer === 'Y') {
+                    rl.question(`\nPlease enter new title\n`, (updatedTitle) => {
+                        list[id].title = updatedTitle;
+                        (0, index_1.promptOptions)();
+                    });
+                }
+            });
+            rl.question(`\nUpdate note? Y or N\n`, (answer) => {
+                if (answer === 'Y') {
+                    rl.question(`\nPlease enter new note\n`, (updatedNote) => {
+                        list[id].note = updatedNote;
+                        (0, index_1.promptOptions)();
+                    });
+                }
+            });
+        });
+    }),
+        1; //ASYNC TEMP WORKAROUND
 };
 exports.updateTask = updateTask;
--- helpers/update.ts
+++ helpers/update.ts
@@ -6,11 +6,38 @@ const rl = readline.createInterface({
   terminal: false,
 });
 
-import { taskList, promptOptions } from "../index";
+import { Task, promptOptions } from "../index";
 
 //Helper function that edits an existing task
-export const updateTask = (): void => {
-  console.log(taskList);
+export const updateTask = (list: {[key:number]: Task}): void => {
+    //Show user current taskList
+    console.log(list);
 
-  rl.question(`\nPlease enter a task ID\n`, (id: string) => {});
-};
+    //ASYNC TEMP WORKAROUND
+    setTimeout(() => {
+      //Ask user to input id for task to be edited
+      rl.question(
+        `\nPlease select which task you'd like to edit\n`,
+        (id: number) => {
+          console.log(list[id]);
+          rl.question(`\nUpdate title? Y or N\n`, (answer: string) => {
+            if (answer === 'Y') {
+              rl.question(`\nPlease enter new title\n`, (updatedTitle: string) => {
+                list[id].title = updatedTitle
+                promptOptions();
+              })
+            }
+          })
+          rl.question(`\nUpdate note? Y or N\n`, (answer: string) => {
+            if (answer === 'Y') {
+              rl.question(`\nPlease enter new note\n`, (updatedNote: string) => {
+                list[id].note = updatedNote;
+                promptOptions();
+              })
+            }
+          })
+        }
+      );
+    }),
+      1; //ASYNC TEMP WORKAROUND
+}
