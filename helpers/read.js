c7d1ddd49f5114aad5e5df5e29da3a953267271a
Viktor Ristic <vktr.r2@protonmail.com>
2023-07-22 11:53:43 -0400

    Add comments to readTasks function

--- helpers/read.js
+++ helpers/read.js
@@ -8,9 +8,17 @@ var rl = readline.createInterface({
     terminal: false,
 });
 var index_1 = require("../src/index");
+/**
+* Function to read tasks and then return user to promptOptions
+*
+* @param {Object} list The tasks to be read. The keys are numbers, and the values are Task objects.
+* @return {Promise} A Promise that resolves to void and then calls promptOptions.
+*/
 var readTasks = function (list) {
     return new Promise(function (resolve) {
+        //Log current taskList for user
         console.log(list);
+        //Delay 4 sec before prompting user to pick new option again
         setTimeout(resolve, 4000);
     }).then(function () { return (0, index_1.promptOptions)(); });
 };
--- helpers/read.ts
+++ helpers/read.ts
@@ -8,9 +8,18 @@ const rl = readline.createInterface({
 
 import { promptOptions, Task } from "../src/index";
 
+/**
+* Function to read tasks and then return user to promptOptions
+*
+* @param {Object} list The tasks to be read. The keys are numbers, and the values are Task objects.
+* @return {Promise} A Promise that resolves to void and then calls promptOptions.
+*/
+
 export const readTasks = (list: { [key: number]: Task }) => {
   return new Promise<void>((resolve) => {
+    //Log current taskList for user
     console.log(list);
+    //Delay 4 sec before prompting user to pick new option again
     setTimeout(resolve, 4000);
   }).then(() => promptOptions());
 };
