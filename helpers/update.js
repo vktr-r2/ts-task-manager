93c9ed7b7b8bac13711df757d97a45a2da269a67
Viktor Ristic <vktr.r2@protonmail.com>
2023-07-30 21:23:32 -0400

    Add getValidInput function

--- helpers/update.js
+++ helpers/update.js
@@ -1,55 +1,38 @@
 "use strict";
-// const readline = require("readline");
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.updateTask = void 0;
-// export const rl = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout,
-//   terminal: false,
-// });
 const readlineSync = require("readline-sync");
 const index_1 = require("../src/index");
+const getValidInput = (question) => {
+    const input = readlineSync.question(question).toUpperCase();
+    if (input !== "Y" && input !== "N") {
+        console.log("Invalid input.  Please enter 'Y' or 'N'.");
+        return getValidInput(question);
+    }
+    return input;
+};
 const updateTask = (list) => {
     console.log(list);
     const id = readlineSync.question("\nPlease enter ID for task you would like to update\n");
     console.log(list[id]);
     const fieldsToUpdate = [
-        { key: 'title', message: 'Please enter new title\n' },
-        { key: 'note', message: 'Please enter new note\n' },
-        { key: 'dueDate', message: 'Please enter new due date\n' },
+        { key: "title", message: "Please enter new title\n" },
+        { key: "note", message: "Please enter new note\n" },
+        { key: "dueDate", message: "Please enter new due date\n" },
     ];
     for (let field of fieldsToUpdate) {
-        const updateField = readlineSync.question(`\nWould you like to update the ${field.key}? (Y or N)\n`);
+        const updateField = getValidInput(`\nWould you like to update the ${field.key}?  (Y or N)\n`);
         if (updateField === "Y") {
-            list[id][field.key] = readlineSync.question(`\n${field.message}\n`);
+            const key = field.key;
+            const newFieldValue = readlineSync.question(`\n${field.message}\n`);
+            if (key !== "id" && key !== 'dueDate') {
+                list[id][key] = newFieldValue;
+            }
+            else if (key === "dueDate") {
+                list[id][key] = new Date(newFieldValue);
+            }
         }
     }
-    // const updateTitle: string = readlineSync.question(
-    //   "\nWould you like to update the title?\n"
-    // );
-    // if (updateTitle === "Y") {
-    //   list[id].title = readlineSync.question("\nPlease enter new title\n");
-    // }
-    // const updateNote: string = readlineSync.question(
-    //   "\nWould you like to update the note?\n"
-    // );
-    // if (updateNote === "Y") {
-    //   list[id].note = readlineSync.question("\nPlease enter new note\n");
-    // }
-    // const updateStatus: string = readlineSync.question(
-    //   "\nWould you like to update the status?\n"
-    // );
-    // if (updateStatus === "Y") {
-    //   list[id].status === "Incomplete"
-    //     ? (list[id].status = "Complete")
-    //     : (list[id].status = "Incomplete");
-    // }
-    // const updateDueDate: string = readlineSync.question(
-    //   "\nWould you like to update the due date?\n"
-    // );
-    // if (updateDueDate === "Y") {
-    //   list[id].dueDate = readlineSync.question("\nPlease enter new due date\n");
-    // }
     console.log(list[id]);
     setTimeout(() => (0, index_1.promptOptions)(), 4000);
 };
--- helpers/update.ts
+++ helpers/update.ts
@@ -1,14 +1,15 @@
-// const readline = require("readline");
-
-// export const rl = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout,
-//   terminal: false,
-// });
-
 const readlineSync = require("readline-sync");
 
 import { Task, promptOptions } from "../src/index";
+
+const getValidInput = (question: string): string => {
+  const input: string = readlineSync.question(question).toUpperCase();
+  if (input !== "Y" && input !== "N") {
+    console.log("Invalid input.  Please enter 'Y' or 'N'.");
+    return getValidInput(question);
+  }
+  return input;
+};
 
 export const updateTask = (list: { [key: string]: Task }): void => {
   console.log(list);
@@ -18,56 +19,29 @@ export const updateTask = (list: { [key: string]: Task }): void => {
   );
   console.log(list[id]);
 
-  const fieldsToUpdate: {key: string, message: string}[] = [
-    {key: 'title', message: 'Please enter new title\n'},
-    {key: 'note', message: 'Please enter new note\n'},
-    {key: 'dueDate', message: 'Please enter new due date\n'},
+  const fieldsToUpdate: { key: string; message: string }[] = [
+    { key: "title", message: "Please enter new title\n" },
+    { key: "note", message: "Please enter new note\n" },
+    { key: "dueDate", message: "Please enter new due date\n" },
   ];
 
-  for (let field of fieldsToUpdate ) {
-    {const updateField: string = readlineSync.question(`\nWould you like to update the ${field.key}? (Y or N)\n`);
+  for (let field of fieldsToUpdate) {
+    const updateField: string = getValidInput(
+      `\nWould you like to update the ${field.key}?  (Y or N)\n`
+    );
     if (updateField === "Y") {
       const key = field.key as keyof Task;
-      const newFieldValue: string = readlineSync.question(`\n${field.message}\n`);
-      if (key !== 'id' && key !== 'dueDate') {
+      const newFieldValue: string = readlineSync.question(
+        `\n${field.message}\n`
+      );
+      if (key !== "id" && key !== 'dueDate') {
         list[id][key] = newFieldValue;
-      } else if (key === 'dueDate') {
-        list[id][key] = new Date(newFieldValue);
       }
-    }}
+      else if (key === "dueDate") {
+        list[id][key] = new Date(newFieldValue)
+      }
+    }
   }
-  
-  
-
-  // const updateTitle: string = readlineSync.question(
-  //   "\nWould you like to update the title?\n"
-  // );
-  // if (updateTitle === "Y") {
-  //   list[id].title = readlineSync.question("\nPlease enter new title\n");
-  // }
-
-  // const updateNote: string = readlineSync.question(
-  //   "\nWould you like to update the note?\n"
-  // );
-  // if (updateNote === "Y") {
-  //   list[id].note = readlineSync.question("\nPlease enter new note\n");
-  // }
-
-  // const updateStatus: string = readlineSync.question(
-  //   "\nWould you like to update the status?\n"
-  // );
-  // if (updateStatus === "Y") {
-  //   list[id].status === "Incomplete"
-  //     ? (list[id].status = "Complete")
-  //     : (list[id].status = "Incomplete");
-  // }
-
-  // const updateDueDate: string = readlineSync.question(
-  //   "\nWould you like to update the due date?\n"
-  // );
-  // if (updateDueDate === "Y") {
-  //   list[id].dueDate = readlineSync.question("\nPlease enter new due date\n");
-  // }
 
   console.log(list[id]);
 
