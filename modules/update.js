ca1ea8678233ed2e45abf35781298d8e7a246b6a
Viktor Ristic <vktr.r2@protonmail.com>
2023-08-05 00:01:36 -0400

    Update the updateTask function comments

--- modules/update.js
+++ modules/update.js
@@ -6,10 +6,24 @@ const index_1 = require("../src/index");
 const validateYOrN_1 = require("../helpers/validateYOrN");
 const getValidDate_1 = require("../helpers/getValidDate");
 const validateTaskSelect_1 = require("../helpers/validateTaskSelect");
+/**
+ * Function to update title, note, dueDate on existing tasks
+ * 1. Log existing task list
+ * 2. Receive and validate selected task input
+ * 3. Log existing task that was selected
+ * 4. Introduce object of fields and messages to allow for looping through updates
+ * 5. Nested loops ask user if they want to update, validate input if yes, store input in according task field
+ * 6. Log updated task
+ * 7. Redirect to promptOptions
+ *
+ * @param list
+ * @return void
+ */
 const updateTask = (list) => {
     console.log(list);
     const id = (0, validateTaskSelect_1.validateTaskSelection)(list, "update");
     console.log(list[id]);
+    // Array of objects to loop through title, note and dueDate updates
     const fieldsToUpdate = [
         { key: "title", message: "Please enter new title\n" },
         { key: "note", message: "Please enter new note\n" },
@@ -19,13 +33,18 @@ const updateTask = (list) => {
         },
     ];
     for (let field of fieldsToUpdate) {
+        //Ask if user wanted to update the field
         const updateField = (0, validateYOrN_1.validateYOrN)(`\nWould you like to update the ${field.key}?  (Y or N)\n`);
         if (updateField === "Y") {
+            //Set key string to be keyof Task
             const key = field.key;
+            //Ask user to input new title/note
             if (key === "title" || key === "note") {
                 let newFieldValue = readlineSync.question(`\n${field.message}\n`);
+                //Store new title/note in task
                 list[id][key] = newFieldValue;
             }
+            //If user wanted to update dueDate, call getValidDate function
             if (key === "dueDate") {
                 list[id].dueDate = (0, getValidDate_1.getValidDate)(list);
             }
--- modules/update.ts
+++ modules/update.ts
@@ -5,13 +5,29 @@ import { validateYOrN } from "../helpers/validateYOrN";
 import { getValidDate } from "../helpers/getValidDate";
 import { validateTaskSelection } from "../helpers/validateTaskSelect";
 
+/**
+ * Function to update title, note, dueDate on existing tasks
+ * 1. Log existing task list
+ * 2. Receive and validate selected task input
+ * 3. Log existing task that was selected
+ * 4. Introduce object of fields and messages to allow for looping through updates
+ * 5. Nested loops ask user if they want to update, validate input if yes, store input in according task field
+ * 6. Log updated task
+ * 7. Redirect to promptOptions
+ * 
+ * @param list 
+ * @return void
+ */
+
 export const updateTask = (list: { [key: string]: Task }): void => {
+
   console.log(list);
 
   const id = validateTaskSelection(list, "update");
 
   console.log(list[id]);
 
+  // Array of objects to loop through title, note and dueDate updates
   const fieldsToUpdate: { key: string; message: string }[] = [
     { key: "title", message: "Please enter new title\n" },
     { key: "note", message: "Please enter new note\n" },
@@ -22,19 +38,26 @@ export const updateTask = (list: { [key: string]: Task }): void => {
   ];
 
   for (let field of fieldsToUpdate) {
+
+    //Ask if user wanted to update the field
     const updateField: string = validateYOrN(
       `\nWould you like to update the ${field.key}?  (Y or N)\n`
     );
+
     if (updateField === "Y") {
+      //Set key string to be keyof Task
       const key = field.key as keyof Task;
+
+      //Ask user to input new title/note
       if (key === "title" || key === "note") {
         let newFieldValue: string = readlineSync.question(
           `\n${field.message}\n`
         );
-
+        //Store new title/note in task
         list[id][key] = newFieldValue;
       }
 
+      //If user wanted to update dueDate, call getValidDate function
       if (key === "dueDate") {
         list[id].dueDate = getValidDate(list);
       }
