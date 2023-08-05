1163a90a7277dbb587976105855bf22002aa94ea
Viktor Ristic <vktr.r2@protonmail.com>
2023-08-04 23:22:06 -0400

    Update validateTaskSelect function comments

--- helpers/validateTaskSelect.js
+++ helpers/validateTaskSelect.js
@@ -2,6 +2,17 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.validateTaskSelection = void 0;
 const readlineSync = require("readline-sync");
+/**
+ * Function validates that user has selected an existing task id, and if so returns the id as string
+ * 1. Ask readlineSync.question using action arguement as question suffix
+ * 2. Store input response in id string
+ * 3. Check if list.id is falsy, if so log error and call function recursively
+ * 4. Return id
+ *
+ * @param {Object} list : The tasks to be read. The keys are numbers, and the values are Task objects.
+ * @param action string : String to be used as template literal in question
+ * @returns id string
+ */
 const validateTaskSelection = (list, action) => {
     const id = readlineSync.question(`\nPlease enter ID for task you would like to ${action}\n`);
     if (!list[id]) {
--- helpers/validateTaskSelect.ts
+++ helpers/validateTaskSelect.ts
@@ -2,12 +2,24 @@ const readlineSync = require("readline-sync");
 
 import { Task } from "../src/index";
 
-export const validateTaskSelection = (list: { [key: string]: Task }, action: string): string => {
+/**
+ * Function validates that user has selected an existing task id, and if so returns the id as string
+ * 1. Ask readlineSync.question using action arguement as question suffix
+ * 2. Store input response in id string
+ * 3. Check if list.id is falsy, if so log error and call function recursively
+ * 4. Return id
+ * 
+ * @param {Object} list : The tasks to be read. The keys are numbers, and the values are Task objects.
+ * @param action string : String to be used as template literal in question
+ * @returns id string
+ */
 
+export const validateTaskSelection = (list: { [key: string]: Task }, action: string): string => {
+  
   const id: string = readlineSync.question(
     `\nPlease enter ID for task you would like to ${action}\n`
   );
-
+  
   if (!list[id]) {
     console.log("Issue with ID input");
     return validateTaskSelection(list, action);
